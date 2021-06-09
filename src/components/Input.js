50131325fa5dab491aca6f47c36f39fd07533426
viktor <vikhulei@gmail.com>
2021-06-09 20:38:42 +0300

    Components added

---
+++ src/components/Input.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+const Input = () => {
+    return <div className="inputSection">
+        <div className="inputContainer">
+        <div className="newWord">
+            hello
+        </div>
+        <div className="timer">
+            30
+        </div>
+        <div className="input">
+            <input type="text"></input>
+            <button>Start</button>
+        </div>
+
+    </div>
+    </div>
+    
+}
+
+export default Input
---
+++ src/components/Results.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+const Results = () => {
+    return <div className="resultsContainer">
+        <div className="countCorrect">
+            Correct answers: 10
+        </div>
+        <div className="wordsContainer">
+            <div className="correctWords">
+                <div className="correctWord">
+                    hello
+                </div>
+            </div>
+            <div className="wrongWords">
+                <div className="wrongWord">
+                    fdfdf
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Results
